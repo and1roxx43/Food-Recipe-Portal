9a1e3e091c471f3459ec990de30ff214d456384c
and1roxx43 <and1roxx43@gmail.com>
2021-04-02 23:56:07 +1100

    First initialisation, Google map, location, and restaurants near by

---
+++ assets/app.js
@@ -0,0 +1,223 @@
+const mexican = document.getElementById('mexican');
+const asian = document.getElementById('asian');
+const italian = document.getElementById('italian');
+const indian = document.getElementById('indian');
+const comfort = document.getElementById('comfort');
+const closeMap = document.getElementById('close-map');
+
+const mapContainer = document.querySelector('.map-container');
+
+const mex = document.querySelector('.mex');
+const ital = document.querySelector('.italian');
+const asia = document.querySelector('.asian');
+const indi = document.querySelector('.indian');
+const burger = document.querySelector('.comfort');
+const findNearBy = document.querySelectorAll('.btn');
+
+var map;
+
+mex.addEventListener('click', function(){
+    mexican.setAttribute('style', 'display: inline;');
+    ital.style.pointerEvents = 'none';
+    asia.style.pointerEvents = 'none';
+    indi.style.pointerEvents = 'none';
+    burger.style.pointerEvents = 'none';
+    // window.document.body.style.pointerEvents = 'none';
+});
+
+ital.addEventListener('click', function(){
+    italian.setAttribute('style', 'display: inline;');
+    mex.style.pointerEvents = 'none';
+    asia.style.pointerEvents = 'none';
+    indi.style.pointerEvents = 'none';
+    burger.style.pointerEvents = 'none';
+});
+
+asia.addEventListener('click', function(){
+    asian.setAttribute('style', 'display: inline;');
+    mex.style.pointerEvents = 'none';
+    ital.style.pointerEvents = 'none';
+    indi.style.pointerEvents = 'none';
+    burger.style.pointerEvents = 'none';
+});
+
+indi.addEventListener('click', function(){
+    indian.setAttribute('style', 'display: inline;');
+    mex.style.pointerEvents = 'none';
+    asia.style.pointerEvents = 'none';
+    ital.style.pointerEvents = 'none';
+    burger.style.pointerEvents = 'none';
+});
+
+burger.addEventListener('click', function(){
+    comfort.setAttribute('style', 'display: inline;');
+    mex.style.pointerEvents = 'none';
+    asia.style.pointerEvents = 'none';
+    indi.style.pointerEvents = 'none';
+    ital.style.pointerEvents = 'none';
+});
+
+
+// click on find a near by restaurants
+mexican.addEventListener('click', function(){
+
+mapContainer.style.display = "flex";
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: latitude, lng: longitude},
+        zoom: 12,
+      });
+      var request = {
+        location: map.getCenter(),
+        query: 'mexican',
+        radius: 5000,
+        type: ['restaurant']
+      }
+    
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.textSearch(request, callback);
+
+});
+
+
+asian.addEventListener('click', function(){
+
+    mapContainer.style.display = "flex";
+
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: latitude, lng: longitude},
+        zoom: 12,
+      });
+      var request = {
+        location: map.getCenter(),
+        query: 'chinese',
+        radius: 5000,
+        type: ['restaurant']
+      }
+    
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.textSearch(request, callback);
+
+});
+
+italian.addEventListener('click', function(){
+
+    mapContainer.style.display = "flex";
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: latitude, lng: longitude},
+        zoom: 12,
+      });
+      var request = {
+        location: map.getCenter(),
+        query: 'italian',
+        radius: 5000,
+        type: ['restaurant']
+      }
+    
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.textSearch(request, callback);
+
+});
+
+indian.addEventListener('click', function(){
+
+    mapContainer.style.display = "flex";
+
+    map = new google.maps.Map(document.getElementById("map"), {
+
+        center: { lat: latitude, lng: longitude},
+        zoom: 12,
+      });
+      var request = {
+        location: map.getCenter(),
+        query: 'indian',
+        radius: 5000,
+        type: ['restaurant']
+      }
+    
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.textSearch(request, callback);
+
+});
+
+comfort.addEventListener('click', function(){
+
+    mapContainer.style.display = "flex";
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: latitude, lng: longitude},
+        zoom: 12,
+      });
+      var request = {
+        location: map.getCenter(),
+        query: 'burger',
+        radius: 5000,
+        type: ['restaurant']
+      }
+    
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.textSearch(request, callback);
+
+});
+
+
+
+// ---------------------------
+function success(pos){
+    var crd = pos.coords;
+    latitude = crd.latitude;
+    console.log(latitude);
+    longitude = crd.longitude;
+    console.log(longitude);
+  
+    var latLong = pos.coords.latitude + ', ' + pos.coords.longitude;
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  
+
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results.length);
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    label: place.name
+    
+  })
+  
+}
+
+closeMap.addEventListener('click', function(){
+
+    mapContainer.style.display = 'none';
+
+    for(var i = 0; i < findNearBy.length; i++){
+        findNearBy[i].style.display = 'none';
+    }
+    location.reload();
+
+
+})
